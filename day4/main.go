ed3f0d480e42b30f3e38e1b6c50b897af7df7b00
Saku Karttunen <saku.karttunen@gmail.com>
2024-12-04 17:27:56 +0200

    wacky little if statement hell

--- day4/main.go
+++ day4/main.go
@@ -3,45 +3,72 @@ package main
 import (
 	"fmt"
 	"sakuexe/adventofcode2024/utils"
-	"strconv"
 )
 
-var solutionMap = make(map[int]string)
+var solutionMap = make(map[int]rune)
 
 func main() {
-  solutionMap[0] = "X"
-  solutionMap[1] = "M"
-  solutionMap[2] = "A"
-  solutionMap[3] = "S"
+	solutionMap[0] = 'X'
+	solutionMap[1] = 'M'
+	solutionMap[2] = 'A'
+	solutionMap[3] = 'S'
 
-  inputText := utils.ReadFile("day4/input.test")
+	inputText := utils.ReadFile("day4/input.test")
 
-  fmt.Println("XMAS")
-  fmt.Printf("found %d hits in row\n", checkRow(inputText[0]))
+	result := solveSearch(inputText)
+	fmt.Printf("found %d hits in row\n", result)
 }
 
-func solveSearch(wordSearch []string) {
+func solveSearch(wordSearch []string) int {
+	totalScore := 0
+	for index, row := range wordSearch {
+		totalScore += checkRow(row)
+		if checkRow(row) != 0 {
+			fmt.Printf("line: #%d has a hit\n", index)
+		}
+	}
+	return totalScore
 }
 
 func checkRow(row string) int {
-  foundInRow := 0
+	foundInRow := 0
+	forward := []rune{}
+	backward := []rune{}
 
-  forward := ""
-  for _, character := range row {
-    fmt.Printf("current character is: %c\n", character)
-    fmt.Printf("wanted character: %s\n", solutionMap[len(forward)])
-    fmt.Println("forward value: ", forward)
-    currentChar := strconv.QuoteRune(character)
-    if solutionMap[len(forward)] != currentChar {
-      fmt.Println("[chars do not match]")
-      forward = ""
-      continue
-    }
-    forward += currentChar
-    if forward == "XMAS" {
-      foundInRow += 1
-    }
-  }
+	for _, character := range row {
 
-  return foundInRow
+		// handle the forward search
+		if solutionMap[len(forward)] != character {
+			// reset the count, unless its an x
+			if character == 'X' {
+				forward = []rune{'X'}
+			} else {
+				forward = []rune{}
+			}
+		} else {
+			forward = append(forward, character)
+			if len(forward) == 4 {
+				foundInRow += 1
+				forward = []rune{}
+			}
+		}
+
+		// handle the reverse search
+		if solutionMap[3 - len(backward)] != character {
+			// reset the count, unless its an x
+			if character == 'S' {
+				backward = []rune{'S'}
+			} else {
+				backward = []rune{}
+			}
+		} else {
+			backward = append(backward, character)
+			if len(backward) == 4 {
+				foundInRow += 1
+				backward = []rune{}
+			}
+		}
+	}
+
+	return foundInRow
 }
